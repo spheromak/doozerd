c5e0d8e0a830ef142611f75445cfc164d53d1525
Jesse Nelson <spheromak@gmail.com>
2013-04-20 16:51:48 -0700

    update service recipe with the template and service code

--- recipes/_service.rb
+++ recipes/_service.rb
@@ -7,7 +7,22 @@
 #
 
 
-
-template "/etc/init.d/doozerd" do
+template "#{node[:doozerd][:init_path]}/doozerd" do
+  notifies :restart, "service[doozerd]"
+  source "doozer_init.erb"
   mode 0755
+  # for now use the attribute, some wrapper could use search/discovery 
+  # and probably should.
+  variables( 
+    :doozer_nodes => node[:doozerd][:nodes],
+    :doozer_host  => node[:doozerd][:host],
+    :doozer_port  => node[:doozerd][:port]
+  )
 end
+
+
+service "doozerd" do 
+  action :start
+end
+
+
