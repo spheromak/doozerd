1e2ea6021d9ca82caccda52ea6a0eef560d55a4a
Jesse Nelson <spheromak@gmail.com>
2013-04-20 16:51:19 -0700

    add init script and node variables

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,10 @@ default[:doozerd][:bin_path] = "/usr/local/bin"
 default[:doozerd][:bin_uri] = "http://dl.dropbox.com/u/848501/doozerd"
 default[:doozerd][:version] = "02072012"
 default[:doozerd][:md5sum] = "1a2058564ae5557a246d7e6f04305bfc"
+
+default[:doozerd][:nodes] = "127.0.0.1"
+default[:doozerd][:host] = "127.0.0.1"
+default[:doozerd][:port] = "9200"
+
+default[:doozerd][:init_path] = "/etc/init.d"
+
