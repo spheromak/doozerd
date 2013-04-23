daf487a33c2d5fa847240d08d798f80770fca228
Jesse Nelson <spheromak@gmail.com>
2013-04-23 14:40:03 -0700

    update doozerd/doozer attribs in their own files

---
+++ attributes/doozer.rb
@@ -0,0 +1,4 @@
+default[:doozer][:prefix] = "/usr/local/bin"
+default[:doozer][:bin_uri] = "http://dl.dropbox.com/u/848501/doozer"
+default[:doozer][:version] = "04232013"
+default[:doozer][:md5sum] = "63a32ee2421d6e67086d8359a8b44720"
---
+++ attributes/doozerd.rb
@@ -0,0 +1,12 @@
+default[:doozerd][:prefix] = "/usr/local/bin"
+default[:doozerd][:bin_uri] = "http://dl.dropbox.com/u/848501/doozerd"
+default[:doozerd][:version] = "04232013"
+default[:doozerd][:md5sum] = "ab781827b471ecf4322cf06b7d80895e"
+
+default[:doozerd][:nodes] = "127.0.0.1"
+default[:doozerd][:host] = "127.0.0.1"
+default[:doozerd][:port] = "9200"
+
+default[:doozerd][:init_path] = "/etc/init.d"
+
+
