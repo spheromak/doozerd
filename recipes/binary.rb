77a7913d95b93ed67f93bc8f3de6d65c74a8b5b2
Jesse Nelson <spheromak@gmail.com>
2013-04-23 20:08:27 -0700

    get the client ont he box as well binary install working

--- recipes/binary.rb
+++ recipes/binary.rb
@@ -8,31 +8,50 @@
 #  This is a quick hack recipe and is intended to be replaced by a proper package recipe.
 #  
 
-doozerd_bin = node[:doozerd][:bin_path] + "/doozerd"
 
-case node[:os] 
-when "linux" 
-  os = "linux"
-  arch = node[:kernel][:machine]
-  bin_url = "#{node[:doozerd][:bin_uri]}-#{os}-#{arch}-#{node[:doozerd][:version]}"
-else 
-  raise AttributeError, "Unsupported platform for doozerd binary install"
-end
-
-remote_file doozerd_bin do
-  action :nothing
-  source bin_url
-  mode 0755
-  backup false
-  checksum node[:doozerd][:md5sum]
-end
-
-http_request "HEAD #{bin_url}" do
-  message ""
-  url bin_url
-  action :head
-  if File.exists?(doozerd_bin)
-    headers "If-Modified-Since" => File.mtime(doozerd_bin).httpdate
+# TODO: these methods should be in libs
+def doozer_uri(app)
+  case node[:os] 
+  when "linux" 
+    os = "linux"
+    arch = node[:kernel][:machine]
+    uri = "#{node[app][:bin_uri]}-#{os}-#{arch}-#{node[:doozerd][:version]}"
+  else 
+    raise AttributeError, "Unsupported platform for doozerd binary install"
   end
-  notifies :create, "remote_file[#{doozerd_bin}]", :immediately
+  uri
+end
+
+def doozer_bin(app)
+  "#{node[app][:prefix]}/#{app}"
+end
+
+def doozer_sum(app)
+  node[app][:md5sum]
+end
+
+%w/doozerd doozer/.each do |app| 
+  # do this here so we don't have to polute the rsources with our methods
+  uri = doozer_uri(app)
+  bin = doozer_bin(app)
+  sum = doozer_sum(app)
+
+  Chef::Log.debug "computed doozer values:\nuri: #{uri}\nbin: #{bin}\nsum: #{sum}"
+  remote_file bin do
+    action :nothing
+    source uri
+    mode 0755
+    backup false
+    checksum sum
+  end
+
+  http_request "HEAD " + uri do
+    message ""
+    url uri
+    action :head
+    if File.exists?( bin )
+      headers "If-Modified-Since" => File.mtime(bin).httpdate
+    end
+    notifies :create, "remote_file[#{bin}]", :immediately
+  end
 end
